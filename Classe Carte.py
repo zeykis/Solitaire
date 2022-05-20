da29a26874c98e7d482f7a62ad3b2d9ce437d571
zeykis <92688032+zeykis@users.noreply.github.com>
2022-05-20 12:06:52 +0200

    Added start of tkinter display

--- Classe Carte.py
+++ Classe Carte.py
@@ -1,3 +1,38 @@
+from cgitb import grey
+import sys
+import random
+from tkinter import *
+racine=Tk()
+class Solitaire:
+    def __init__(self,n=racine):
+        ##Initialisation de nos objets nécessaires pour jouer
+        self.deck=piledecarte()
+        self.tpA=tableauPile()
+        self.tpB=tableauPile()
+        self.tpC=tableauPile()
+        self.tpD=tableauPile()
+        self.tpE=tableauPile()
+        self.tpF=tableauPile()
+        self.tpG=tableauPile()
+        self.tableau=[self.tpA,self.tpB,self.tpC,self.tpD,self.tpE,self.tpF,self.tpG]
+        self.objectif_coeur=objectifPile(suite.coeur)
+        self.objectif_carreau=objectifPile(suite.carreau)
+        self.objectif_pique=objectifPile(suite.pique)
+        self.objectif_trefle=objectifPile(suite.trefle)
+        self.tirage=tirage()
+        self.ecarter_carte=ecarter()
+        self.temp=transition()
+        self.distribuerCarte()
+
+        #Grille
+        self.ligneHaut = 2
+        self.cadre= Frame(Tk(),height=640,width=640,bg=grey)
+
+        Label(self.cadre, text="Solitaire d'Antoine et Thibault",bg="grey").grid(column=0,row=0,columnspan=15)
+        
+    
+
+
 class Deck:
     def __init__(self):
         self.statut = "Rien"
